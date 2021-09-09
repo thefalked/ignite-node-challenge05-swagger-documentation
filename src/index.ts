2e40b205ca79b77dec6c7343471369397c931cef
thefalked <giulianocrivelli@hotmail.com>
2021-09-09 18:07:33 -0400

    finished challenge

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import swaggerIU from "swagger-ui-express";
 
+import swaggerFile from "../swagger.json";
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerIU.serve, swaggerIU.setup(swaggerFile));
 
 app.use("/users", usersRoutes);
 
